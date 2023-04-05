b261655fea5b92b65ce8f71cd4a1eb9c1cc2f1ec
Maximiliano Santana <iimaxsany@gmail.com>
2023-04-05 09:17:19 -0600

    Se mejora el disenio del carrito y se integra al index.html

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const desktopMenuActivate = document.querySelector('.account-menu')
 const desktopMenu = document.querySelector('.desktop-menu')
-const MovileMenuActivate = document.querySelector('#menu-icon')
-const MovileMenu = document.querySelector('.movile-menu')
+const movileMenuActivate = document.querySelector('#menu-icon')
+const movileMenu = document.querySelector('.movile-menu')
+const shoppingCartActivate = document.querySelector('.shopping-cart-icon')
+const shoppingCart = document.querySelector('.my-shopping-cart')
 
 console.log(desktopMenu.classList)
 
 desktopMenuActivate.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive')
+    shoppingCart.classList.add('inactive')
 })
 
-MovileMenuActivate.addEventListener('click', () => {
-    console.log('abremenu')
-    MovileMenu.classList.toggle('inactive')
+movileMenuActivate.addEventListener('click', () => {
+    movileMenu.classList.toggle('inactive')
+    shoppingCart.classList.add('inactive')
+})
+
+shoppingCartActivate.addEventListener('click', () => {
+    shoppingCart.classList.toggle('inactive')
+    movileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    console.log('abbre carrito')
 })
