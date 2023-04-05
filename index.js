7bf8f4d410c9abe6016a1110b8486ea659732c6b
Maximiliano Santana <iimaxsany@gmail.com>
2023-04-05 12:12:42 -0600

    Se crea una lista de productos en el js y su constructor para crear cada producto en la lista, tambien se crea la funcion para renderizar cada producto en el html

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const movileMenuActivate = document.querySelector('#menu-icon')
 const movileMenu = document.querySelector('.movile-menu')
 const shoppingCartActivate = document.querySelector('.shopping-cart-icon')
 const shoppingCart = document.querySelector('.my-shopping-cart')
+const cardsContainer = document.querySelector('.cards-container')
 
+const productList = [];
+
+class Product {
+    constructor(name, price, image) {
+        this.name = name
+        this.price = price
+        this.image = image
+    }
+}
+
+productList.push(new Product('Hoodie', 230, 'https://images.pexels.com/photos/1749495/pexels-photo-1749495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'));
+productList.push(new Product('Bike', 230, 'https://images.pexels.com/photos/1749495/pexels-photo-1749495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'));
 
 desktopMenuActivate.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive')
@@ -20,4 +33,21 @@ shoppingCartActivate.addEventListener('click', () => {
     shoppingCart.classList.toggle('inactive')
     movileMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive')
+})
+
+productList.forEach((product) => {
+    cardsContainer.innerHTML += `
+    <div class="product-card">
+        <div class="image-card">
+            <img src="${product.image}">
+        </div>
+        <div class="footer-product-card">
+            <div class="data-product-card">
+                <p class="product-card-name">${product.name}</p>
+                <p class="product-card-price">${product.price} $</p>
+            </div>
+            <img src="img/shopping-cart.png" alt="" id="shopping-car-image">
+        </div>
+    </div>
+    `
 })
