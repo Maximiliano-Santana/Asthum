2d8b9dcd5a7d7f9b924b9c402a409ce820a9e74e
Maximiliano Santana <iimaxsany@gmail.com>
2023-04-04 20:55:37 -0600

    Se crea el despliegue del menu para movile y arreglan bugs de responsive design al achicar la pantalla

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const desktopMenuActivate = document.querySelector('.account-menu')
 const desktopMenu = document.querySelector('.desktop-menu')
+const MovileMenuActivate = document.querySelector('#menu-icon')
+const MovileMenu = document.querySelector('.movile-menu')
 
 console.log(desktopMenu.classList)
+
 desktopMenuActivate.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive')
+})
+
+MovileMenuActivate.addEventListener('click', () => {
+    console.log('abremenu')
+    MovileMenu.classList.toggle('inactive')
 })
