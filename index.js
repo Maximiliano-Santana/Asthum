58ea950897af4de275a131528a5e444d80cb5dfa
Maximiliano Santana <iimaxsany@gmail.com>
2023-04-05 21:50:17 -0600

    Se integran los detalles del producto

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const movileMenu = document.querySelector('.movile-menu')
 const shoppingCartActivate = document.querySelector('.shopping-cart-icon')
 const shoppingCart = document.querySelector('.my-shopping-cart')
 const cardsContainer = document.querySelector('.cards-container')
+const productDetailsActivate = document.querySelector('.cards-container')
+const productDetailsClose = document.querySelector('.close-icon-product-details')
+const productDetails = document.querySelector('.product-details')
 
 const productList = [];
 
@@ -22,32 +25,55 @@ productList.push(new Product('Bike', 230, 'https://images.pexels.com/photos/1749
 desktopMenuActivate.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive')
     shoppingCart.classList.add('inactive')
+    productDetails.classList.add('inactive')
 })
 
 movileMenuActivate.addEventListener('click', () => {
     movileMenu.classList.toggle('inactive')
     shoppingCart.classList.add('inactive')
+    productDetails.classList.add('inactive')
 })
 
 shoppingCartActivate.addEventListener('click', () => {
     shoppingCart.classList.toggle('inactive')
     movileMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive')
+    productDetails.classList.add('inactive')
 })
 
-productList.forEach((product) => {
-    cardsContainer.innerHTML += `
-    <div class="product-card">
-        <div class="image-card">
-            <img src="${product.image}">
-        </div>
-        <div class="footer-product-card">
-            <div class="data-product-card">
-                <p class="product-card-name">${product.name}</p>
-                <p class="product-card-price">${product.price} $</p>
+productDetailsActivate.addEventListener('click', () => {
+    productDetails.classList.remove('inactive')
+})
+productDetailsClose.addEventListener('click', () => {
+    productDetails.classList.add('inactive')
+})
+
+
+
+function renderProducts() {
+    productList.forEach((product) => {
+        cardsContainer.innerHTML += `
+        <div class="product-card">
+            <div class="image-card">
+                <img src="${product.image}">
             </div>
-            <img src="img/shopping-cart.png" alt="" id="shopping-car-image">
+            <div class="footer-product-card">
+                <div class="data-product-card">
+                    <p class="product-card-name">${product.name}</p>
+                    <p class="product-card-price">${product.price} $</p>
+                </div>
+                <div class="right-footer-card">
+                    +
+                    <img src="img/shopping-cart.png" alt="" id="shopping-car-image">
+                </div>
+            </div>
         </div>
-    </div>
-    `
+        `
+    })
+}
+
+renderProducts()
+const addShoppingCart = document.querySelector('.right-footer-card')
+addShoppingCart.addEventListener('click', () => {
+    console.log('se agrego producto al carrito')
 })
